3da1bfec9210f5c47a8931a3a6e6ecddd780648a
sericharan1-bit <sericharan1@gmail.com>
2026-03-24 11:58:32 +0530

    Added home controller

---
+++ src/main/java/com/example/cabsystemsms/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.cabsystemsms;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Smart Farming Backend is running successfully";
+    }
+}
