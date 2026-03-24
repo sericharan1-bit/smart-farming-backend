37573d310f673348f71e92325a9c213846dc1717
sericharan1-bit <sericharan1@gmail.com>
2026-03-24 12:15:22 +0530

    Enabled CORS

--- src/main/java/com/example/cabsystemsms/HomeController.java
+++ src/main/java/com/example/cabsystemsms/HomeController.java
@@ -1,8 +1,10 @@
 package com.example.cabsystemsms;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class HomeController {
 
